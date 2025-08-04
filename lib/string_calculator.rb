2531c3d6793a8a5ab08ad4ceb348ecb4b5ce5769
Rahul Sharma <rahulsharmadholpur40@gmail.com>
2025-08-04 21:04:11 +0530

    feat: implement empty string handling (TDD Step 1)

---
+++ lib/string_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# StringCalculator provides functionality to add numbers from a string input
+class StringCalculator
+  def add(input)
+    return 0 if input.empty?
+  end
+end
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add('')).to eq(0)
+    end
+  end
+end
